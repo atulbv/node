2cab591d810dbce9ac136f95c3e776aee85d1d34
Atul <vernekaratul@gmail.com>
2021-06-06 15:56:05 +0530

    Commit hbs file for UI

--- Books/books.js
+++ Books/books.js
@@ -2,19 +2,31 @@
 const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
+const path = require("path");
+const hbs = require('hbs');
 
 app.use(
     express.urlencoded({
-      extended: true
+      extended: false
     })
   )
 
 app.use(express.json())
 app.use(bodyparser.json());
+const publicPath = path.join(__dirname, '../public');
 
+const template_path= path.join(__dirname,"../Views")
 
 const mongoose = require("mongoose");
 const { response } = require("express");
+
+
+app.use(express.static(publicPath));
+
+app.set('view engine', 'hbs');
+
+app.set("Views", template_path);
+
 
 require("./book")
 
@@ -22,59 +34,19 @@ const Book = mongoose.model("Book")
 const CloudDBuri = "mongodb+srv://atul:Atulpostman@15@cluster0.vzqih.mongodb.net/bookService_atul?retryWrites=true&w=majority";
 const localDBUri = "mongodb://localhost:27017/AtulBookStore";
 mongoose.connect(CloudDBuri,{ useNewUrlParser: true, useUnifiedTopology: true }).then(() =>{
-    console.log("mongoose DB is connected..")
+    console.log("mongoose DB is connected..");   
+   
 }).catch((err) => console.log(err));
 
-//Cloud based mongoDB
-// const MongoClient = require('mongodb').MongoClient;
-
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect().then(() => {
-//     //const collection = client.db("bookService_atul").collection("Atul");
-//     console.log("DB is connected.."); 
-    
-//   })
-// .catch(err => {
-//     console.error('App starting error:', err.stack);
-//     process.exit(1)
-// });
-
-app.post("/book",  (req,res)=>{
-   var newBook = {
-    title: req.body.title,
-    author: req.body.author,
-    numberPages: req.body.numberPages,
-    publisher: req.body.publisher
-    }
-   var book = new Book(newBook)
-
-
-     book.save().then(() => {
-        console.log("New book created")
-    }).catch((err) => {
-        if(err){
-            // Here, what happens if the err variable is not empty?
-            // The done function is never called here, hence it may never finish (timeout)
-            console.log(err);
-          } else{
-           
-          }
-
-   }) 
-   res.send("New book created with success..")
-    // console.log(req.body);
-    // res.send("post book service");
-
-})
 
 app.get("/", (req,res) => {
+  res.render("index")
+})
 
-console.log("Welcome to atul book store from console.")
-  // Book.find().then((books)=> {
-  //  // console.log(books)
-  //  res.json(books)
-  // }).catch((err) => { console.log(err)})
-   res.send("Welcome to atul book store");
+app.get("/addBook", (req,res) => {
+
+    res.render("index")
+ 
 })
 
 app.get("/book/:id", (req,res) => {
@@ -90,6 +62,36 @@ app.get("/book/:id", (req,res) => {
   }).catch((err) => console.log(err));
    
 })
+
+
+app.post("/addBook",  (req,res)=>{
+  var newBook = {
+   title: req.body.title,
+   author: req.body.author,
+   numberPages: req.body.numberPages,
+   publisher: req.body.publisher
+   }
+  var book = new Book(newBook)
+
+
+    book.save().then(() => {
+       console.log("New book created")
+   }).catch((err) => {
+       if(err){
+          
+           console.log(err);
+         } else{
+          
+         }
+
+  }) 
+  res.send("New book created with success..")
+   // console.log(req.body);
+   // res.send("post book service");
+
+})
+
+
 const port = process.env.port || 4200;
 app.listen(port,()=> {
 console.log("book service is running");
